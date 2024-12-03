afde1febadb193301e0dffd6539d777ba3bc3b8c
lim <mylim52@naver.com>
2024-12-03 15:55:15 +0900

    1203_02

--- MYLim/w1202/c01/board/views.py
+++ MYLim/w1202/c01/board/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from board.models import Board
+from django.db.models import Q
+from django.core.paginator import Paginator # 페이지 넘버링
 
 # Create your views here.
 def blist(request):
-  return render(request,'blist.html')
+  npage = int(request.GET.get('npage',1))
+  qs = Board.objects.all().order_by('-b_no')
+  cl = request.GET.get('cl','')
+  searchType = request.GET.get('type','b_title')
+  
+  if cl:
+    if searchType == 'b_title':
+      qs = Board.objects.filter(b_title__icontains=cl)
+    elif searchType == 'b_content':
+      qs = Board.objects.filter(b_content__icontains=cl)
+    elif searchType == 'b_title_content':
+      qs = Board.objects.filter(Q(b_title__icontains=cl)|Q(b_content__icontains=cl))
+  
+  paginator = Paginator(qs,2)
+  blist = paginator.get_page(npage)
+  context = {'blist':blist,'npage':npage,'cl':cl,'searchType':searchType}
+  return render(request,'blist.html',context)
--- MYLim/w1202/c01/c01/urls.py
+++ MYLim/w1202/c01/c01/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('board/',include('board.urls'))
+    path('board/',include('board.urls')),
+    path('member/',include('member.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
