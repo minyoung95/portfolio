3c78072b60312cb00fc8900a2bf65f7113bab286
apdlvmfqjqtk <zmgkgzmgkg@gmail.com>
2024-12-05 18:16:33 +0900

    MW

--- Maple_wizard/MW/member/models.py
+++ Maple_wizard/MW/member/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from datetime import datetime
 
 class Member(models.Model):
-  m_username = models.CharField(max_length=100)
   m_id = models.CharField(max_length=50,primary_key=True)
   m_password = models.CharField(max_length=100)
+  m_username = models.CharField(max_length=100)
   m_nickName = models.CharField(max_length=100)
-  m_email = models.CharField(max_length=100)
   m_gender = models.CharField(max_length=10, default='남자')
+  m_email = models.CharField(max_length=100)
   m_date = models.DateTimeField(auto_now=True)
   m_auth = models.IntegerField(default=1)
   
