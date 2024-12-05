ece29c7571632e18531e5cbda0a7fc2485c05f6a
lim <mylim52@naver.com>
2024-12-05 18:30:58 +0900

    1205_bview

--- MYLim/w1202/c01/board/urls.py
+++ MYLim/w1202/c01/board/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('blist/',views.blist,name='blist'),
+    path('likes/',views.likes,name='likes'),
     path('bwrite/',views.bwrite,name='bwrite'),
     path('bview/<int:b_no>/',views.bview,name='bview'),
     path('bupdate/<int:b_no>/',views.bupdate,name='bupdate'),
